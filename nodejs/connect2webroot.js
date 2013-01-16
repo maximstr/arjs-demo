b94b6d41712b9d745a6afec460d9192bebdf16e7
maxim <maxim@videinfra.com>
2013-01-16 16:19:42 +0200

    localhost over https

--- nodejs/connect2webroot.js
+++ nodejs/connect2webroot.js
@@ -1,6 +1,21 @@
-var connect = require('connect'),
-	webroot = process.argv[2] || "webroot";
+var https = require('https');
+var fs = require('fs');
 
-connect.createServer(
-    connect.static(webroot)
-).listen(80);
+
+var options = {
+  cert: fs.readFileSync('cert/certificate.pem').toString(),
+  key: fs.readFileSync('cert/privatekey.pem').toString()
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end(fs.readFileSync('webroot/AR_simple_webgl.html').toString()+"\n");
+}).listen(443);
+
+
+// var connect = require('connect'),
+// 	webroot = process.argv[2] || "webroot";
+
+// connect.createServer(
+//     connect.static(webroot)
+// ).listen(80);
